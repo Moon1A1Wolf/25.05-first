93ab409f5605f462be7669403d210ff27f97430f
Anastasia <126796629+Moon1A1Wolf@users.noreply.github.com>
2023-06-20 13:11:07 +0300

    Create Form1.cs

---
+++ Form1.cs
@@ -0,0 +1,21 @@
+namespace _25_05_23
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.button1.Location = new Point((r.Next(button1.Width, this.Width) - button1.Width), r.Next(button1.Height, this.Height) - button1.Height);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = (r.Next(button1.Width, this.Width) - button1.Width).ToString();
+        }
+    }
+}
